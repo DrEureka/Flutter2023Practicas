03476c2a1fb929cefe1021d848d585c8f587d1b9
Daniel Ramadan <44514691+DrEureka@users.noreply.github.com>
2023-06-18 04:20:02 -0300

    Avances en proyecto 2

---
+++ Curso flutter porfolio/Proyecto 2 - Yes No APP/yes_no_app/lib/config/themes/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+//creo una class nueva llamada AppTheme para pasarle los parametros globales del theme
+class AppTheme {
+  ThemeData theme() {
+    return ThemeData();
+  }
+}
